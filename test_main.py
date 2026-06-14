a94a879d258748c2fdcb7ab99e064554217b8a72
VtthuLoc <vothithuloc11.6@gmail.com>
2026-06-14 19:54:51 +0700

    Create test_main.py

---
+++ test_main.py
@@ -0,0 +1,16 @@
+import unittest
+from main import tinh_diem_gpa
+class TestGpaFunctions(unittest.TestCase):
+  def test_gpa_he_muoi(self):
+    self.assertEqual(tinh_diem_gpa(8.5),4.0)
+  def test_gpa_tuyen_tinh(self):
+    self.assertEqual(tinh_diem_gpa(7.0),2.8)
+  def test_gpa_diem_duoi_trung_binh(self):
+    self.assertEqual(tinh_diem_gpa(4.0),1.6)
+  def test_gpa_diem_am_raise_exception(self):
+    with self.assertEqualRaises(ValueError):
+       tinh_diem_gpa(-1.0)
+if __name__ == '__main__':
+    unittest.main()
+
+    
